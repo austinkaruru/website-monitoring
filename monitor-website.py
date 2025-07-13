aabab1073b4dc82cbbe00f1b2d1e5f1753748c4c
Austin Karuru <austin.karuru@pawait.co.ke>
2025-07-14 00:14:41 +0300

    Added smtp function to send email if site is down

--- monitor-website.py
+++ monitor-website.py
@@ -1,7 +1,32 @@
 import requests
+import smtplib
+import os
+# import dotenv
 
-response = requests.get('http://34.12.84.239:8080/')
-if response.status_code == 200:
-    print("Website is up and running!")
-else:
-    print("Website is down or not reachable")
+# dotenv.load_dotenv()
+
+EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
+PASSKEY = os.environ.get('EMAIL_PASS')
+SERVER = os.environ.get('SERVER')
+
+def send_notification(email_msg):
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.starttls()
+        smtp.ehlo()
+        smtp.login(EMAIL_ADDRESS, PASSKEY)
+        message = f"Subject: Website Not accessible Alert\n{email_msg}"
+        smtp.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, message)
+try: 
+    response = requests.get('http://{SERVER}:8080/')
+    if response.status_code == 200:
+        print("Website is up and running!")
+    else:
+        print("Website is down or not reachable")
+        msg = f"Website returned status code: {response.status_code}"
+        send_notification(msg)
+
+except Exception as ex:
+    print(f'Connection error happened: {ex}')
+    msg = f"An error occurred: {ex}"
+    send_notification(msg)
+    
